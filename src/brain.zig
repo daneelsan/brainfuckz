f03f0e4229c9e25b1cdc428f445020488d99c0cf
Daniel Sanchez <dansando@protonmail.com>
2021-03-18 00:48:15 -0500

    Reworking VM and adding REPL command.

--- src/brain.zig
+++ src/brain.zig
@@ -5,95 +5,107 @@ const stdout = std.io.getStdOut().writer();
 const stdin = std.io.getStdIn().reader();
 const dprint = std.debug.print;
 
-const MEM_SIZE = std.math.maxInt(u16);
-const MAX_CODE_SIZE = std.math.maxInt(u12);
-
 pub const Brain = struct {
-    // allocator: *Allocator,
+    allocator: *Allocator,
 
-    mem: [MEM_SIZE]u8 = [_]u8{0} ** MEM_SIZE,
-    dp: u16 = 0, // data_pointer
-    code: [MAX_CODE_SIZE]u8 = [_]u8{0} ** MAX_CODE_SIZE,
-    ip: u12 = 0, // instruction pointer
+    mem: []u8,
+    dp: usize = 0, // data_pointer
+    // code: []const u8,
+    ip: usize = 0, // instruction pointer
     input: u8 = 0,
     output: u8 = 0,
 
     const Self = @This();
+    pub const defaultMemorySize = 30000;
 
-    // pub fn init(allocator: *Allocator, mem_size: usize) !Self {
-    //     return Self{
-    //         .allocator = allocator,
-    //     };
-    // }
+    pub fn init(allocator: *Allocator, mem_size: usize) !Self {
+        const mem = try allocator.alloc(u8, mem_size);
+        var self = Self{
+            .allocator = allocator,
+            .mem = mem,
+            // .code = &[_]u8{0},
+        };
+        self.clearMemory();
+        return self;
+    }
+
+    pub fn initDefault(allocator: *Allocator) !Self {
+        return try init(allocator, defaultMemorySize);
+    }
+
+    pub fn deinit(self: Self) void {
+        self.allocator.free(self.mem);
+    }
 
     pub fn reset(self: *Self) void {
-        self.resetMemory();
-        self.resetCode();
+        self.clearMemory();
+        // self.resetCode();
         self.dp = 0;
         self.ip = 0;
         self.input = 0;
         self.output = 0;
     }
 
-    pub fn resetMemory(self: *Self) void {
+    pub fn clearMemory(self: *Self) void {
         for (self.mem) |*m| {
             m.* = 0;
         }
     }
 
-    pub fn resetCode(self: *Self) void {
-        for (self.code) |*c| {
-            c.* = 0;
-        }
+    pub const BrainError = error{
+        DataPointerOverflow,
+        DataPointerUnderflow,
+        DataPointerOutOfBounds,
+        InstructionPointerOutOfBounds,
+        LeftBracketUnmatched,
+        RightBracketUnmatched,
+        InputFailed,
+        OutputFailed,
+    };
+
+    pub fn interpret(self: *Self, code: []const u8, n: usize, debug: bool) BrainError!usize {
+        const res = self.run(code, n, debug) catch |err| self.errorHandler(code, err);
+        return res;
     }
 
-    pub fn compile(self: *Self, source: []const u8) usize {
-        self.resetCode();
-        const slice = if (source.len > self.code.len) source[0..self.code.len] else source;
+    pub fn run(self: *Self, code: []const u8, n: usize, debug: bool) BrainError!usize {
+        if (self.ip < 0 or self.ip >= code.len) {
+            return BrainError.InstructionPointerOutOfBounds;
+        }
+        if (self.dp < 0 or self.dp >= self.mem.len) {
+            return BrainError.DataPointerOutOfBounds;
+        }
 
         var i: usize = 0;
-        for (slice) |char| {
-            switch (char) {
-                '>', '<', '+', '-', '[', ']', ',', '.' => {
-                    self.code[i] = char;
-                    i += 1;
-                },
-                else => {},
+        const cond = (n == 0); // 0 means loop forever
+        if (debug) {
+            self.printState(code);
+            while (cond or i < n) : (i += 1) {
+                const offset = try self.execute(code);
+                self.ip = @intCast(usize, @intCast(isize, self.ip) + offset);
+                self.printState(code);
+                if (self.ip == code.len) break;
+            }
+        } else {
+            while (cond or i < n) : (i += 1) {
+                const offset = try self.execute(code);
+                self.ip = @intCast(usize, @intCast(isize, self.ip) + offset);
+                if (self.ip == code.len) break;
             }
         }
         return i;
     }
 
-    pub fn interpret(self: *Self) usize {
-        var i: usize = 0;
-        while (true) : (i += 1) {
-            if (!self.interpretOnce()) break;
-        }
-        return i;
-    }
-
-    pub fn interpretN(self: *Self, n: usize) usize {
-        var i: usize = 0;
-        while (i < n) : (i += 1) {
-            if (!self.interpretOnce()) break;
-        }
-        return i;
-    }
-
-    pub fn interpretOnce(self: *Self) bool {
-        if (self.ip >= self.code.len) return false;
-
-        const instr: u8 = self.code[self.ip];
-        return self.execute(instr);
-    }
-
-    fn execute(self: *Self, instr: u8) bool {
-        switch (instr) {
+    fn execute(self: *Self, code: []const u8) BrainError!isize {
+        var offset: isize = 1;
+        switch (code[self.ip]) {
             '>' => {
-                self.dp +%= 1;
+                if (self.dp == self.mem.len - 1) return BrainError.DataPointerOverflow;
+                self.dp += 1;
             },
             '<' => {
-                self.dp -%= 1;
+                if (self.dp == 0) return BrainError.DataPointerUnderflow;
+                self.dp -= 1;
             },
             '+' => {
                 self.mem[self.dp] +%= 1;
@@ -105,14 +117,15 @@ pub const Brain = struct {
                 // If value of current cell is zero, jump to matching square bracket
                 if (self.mem[self.dp] == 0) {
                     var depth: usize = 1;
-                    while (depth != 0) {
-                        self.ip += 1;
-                        if (self.ip == self.code.len) return false;
+                    var ip: usize = self.ip;
 
-                        switch (self.code[self.ip]) {
+                    while (depth != 0) : (offset += 1) {
+                        if (ip == code.len - 1) return BrainError.LeftBracketUnmatched;
+                        ip += 1;
+
+                        switch (code[ip]) {
                             '[' => depth += 1,
                             ']' => depth -= 1,
-                            0 => return false,
                             else => {},
                         }
                     }
@@ -122,33 +135,46 @@ pub const Brain = struct {
                 // If value of current cell is not zero, loop back to matching square bracket
                 if (self.mem[self.dp] != 0) {
                     var depth: usize = 1;
-                    while (depth != 0) {
-                        self.ip -= 1;
-                        if (self.ip == 0) return false;
+                    var ip: usize = self.ip;
+                    while (depth != 0) : (offset -= 1) {
+                        if (ip == 0) return BrainError.RightBracketUnmatched;
+                        ip -= 1;
 
-                        switch (self.code[self.ip]) {
+                        switch (code[ip]) {
                             '[' => depth -= 1,
                             ']' => depth += 1,
-                            0 => return false,
                             else => {},
                         }
                     }
                 }
             },
             ',' => {
-                const byte = stdin.readByte() catch return false;
+                // TODO: How to read without 'enter'?
+                const byte = stdin.readByte() catch return BrainError.InputFailed;
                 self.mem[self.dp] = byte;
             },
             '.' => {
-                stdout.print("{c}", .{self.mem[self.dp]}) catch return false;
+                stdout.print("{c}", .{self.mem[self.dp]}) catch return BrainError.OutputFailed;
             },
-            else => return false,
+            else => {}, // Ignore other characters
         }
-        self.ip += 1;
-        return true;
+        return offset;
     }
 
-    pub fn printState(self: Self) void {
+    fn errorHandler(self: Self, code: []const u8, err: BrainError) BrainError {
+        dprint("Error [{s}] (code[{d}]: {c}, mem[{d}]: {x:0>2}, in: {c}, out: {c})", .{
+            err,
+            self.ip,
+            code[self.ip],
+            self.dp,
+            self.mem[self.dp],
+            self.input,
+            self.output,
+        });
+        return err;
+    }
+
+    pub fn printState(self: Self, code: []const u8) void {
         comptime const radius = 16;
 
         dprint("\n[\n", .{});
@@ -158,12 +184,12 @@ pub const Brain = struct {
         while (i < self.ip + radius) : (i += 1) {
             if (i < 0) {
                 i = -1;
-            } else if (i > self.code.len) {
+            } else if (i > code.len) {
                 break;
             } else if (i == self.ip) {
-                dprint("({c})", .{self.code[i]});
+                dprint("({c})", .{code[i]});
             } else {
-                dprint("{c}", .{self.code[i]});
+                dprint("{c}", .{code[i]});
             }
         }
         dprint("\n", .{});
@@ -189,8 +215,22 @@ pub const Brain = struct {
         dprint("  output:              '{c}'\n", .{self.output});
         dprint("]\n", .{});
     }
-
-    pub fn printCode(self: Self) void {
-        dprint("Code: {s}\n", .{self.code});
-    }
 };
+
+// Optimizes the code `source` by removing all non-brainfuck characters.
+// Stores the resulting array in `buffer`.
+// pub fn brainCodeOptimize(buffer: []u8, source: []const u8) usize {
+//     const slice = if (source.len > self.code.len) source[0..self.code.len] else source;
+
+//     var i: usize = 0;
+//     for (source) |char| {
+//         switch (char) {
+//             '>', '<', '+', '-', '[', ']', ',', '.' => {
+//                 self.code[i] = char;
+//                 i += 1;
+//             },
+//             else => {},
+//         }
+//     }
+//     return i;
+// }
--- src/main.zig
+++ src/main.zig
@@ -14,6 +14,7 @@ const usage =
     \\ code [BRAIN]     Give brainfuck code to execute
     \\ file [PATH]      Execute the code found in a .brain file
     \\ help             Print this help message and exit
+    \\ test             Enters interactive mode
     \\
 ;
 
@@ -23,10 +24,10 @@ pub fn main() anyerror!void {
 
     const allocator = &arena.allocator;
 
-    const stdout = std.io.getStdOut().writer();
-
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
+
+    const stdout = std.io.getStdOut().writer();
 
     if (args.len <= 1) {
         try stdout.print("{s}", .{usage});
@@ -36,22 +37,44 @@ pub fn main() anyerror!void {
     const command = args[1];
     const commandArgs = args[2..];
 
+    var brain = try Brain.init(allocator, Brain.defaultMemorySize);
+    defer brain.deinit();
+
     if (mem.eql(u8, command, "help")) {
+
         try stdout.print("{s}", .{usage});
+
     } else if (mem.eql(u8, command, "code")) {
+
         const code: []const u8 = commandArgs[0];
 
-        var brain = Brain{};
-        _ = brain.compile(code);
-        const steps = brain.interpret();
+        // _ = brain.compile(code);
+        const steps = try brain.interpret(code, 0, false);
+
     } else if (mem.eql(u8, command, "file")) {
+
         const file = try fs.cwd().openFile(commandArgs[0], .{ .read = true });
         defer file.close();
-        var code = try file.readToEndAlloc(allocator, maxBytesRead);
 
-        var brain = Brain{};
-        _ = brain.compile(code);
-        const steps = brain.interpret();
+        var code = try file.readToEndAlloc(allocator, maxBytesRead);
+        defer allocator.free(code);
+
+        // _ = brain.compile(code);
+        const steps = try brain.interpret(code, 0, false);
+
+    } else if (mem.eql(u8, command, "test")) {
+
+        const stdin = std.io.getStdIn().reader();
+
+        while (true) {
+            try stdout.print("brainfuckz> ", .{});
+            const code = try stdin.readUntilDelimiterAlloc(allocator, '\n', maxBytesRead);
+            defer allocator.free(code);
+            // try stdout.print("{s}", .{code});
+            const steps = try brain.interpret(code, 0, false);
+            try stdout.print("\n", .{});
+        }
+
     } else {
         std.debug.print("{any}", .{args});
     }
